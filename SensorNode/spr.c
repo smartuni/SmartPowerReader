b579f2c387da815bbeeaaf613f779de32c8e524d
Aiman Ismail <m.aimanismail@gmail.com>
2018-10-05 10:40:45 +0200

    initial code for sensor node
    copied and modified from RIOT/examples/gcoap
    
    rename and modify gcoap to spr

---
+++ SensorNode/spr.c
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) 2015-2017 Ken Bannister. All rights reserved.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @author      Ken Bannister <kb2ma@runbox.com>
+ *              M Aiman Ismail <m.aimanismail@gmail.com>
+ *
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "net/gcoap.h"
+#include "od.h"
+#include "fmt.h"
+#include "xtimer.h"
+
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
+#define SPR_INTERVAL (15)       /* Default 15 seconds*/
+
+static void _resp_handler(unsigned req_state, coap_pkt_t* pdu,
+                          sock_udp_ep_t *remote);
+static ssize_t _interval_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx);
+static ssize_t _value_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx);
+
+static uint32_t interval = SPR_INTERVAL;
+
+/* CoAP resources */
+static const coap_resource_t _resources[] = {
+    { "/interval", COAP_GET | COAP_PUT, _interval_handler, NULL },
+    { "/value", COAP_GET, _value_handler, NULL },
+};
+
+static gcoap_listener_t _listener = {
+    &_resources[0],
+    sizeof(_resources) / sizeof(_resources[0]),
+    NULL
+};
+
+/*
+ * Response callback.
+ */
+static void _resp_handler(unsigned req_state, coap_pkt_t* pdu,
+                          sock_udp_ep_t *remote)
+{
+    (void)remote;       /* not interested in the source currently */
+
+    if (req_state == GCOAP_MEMO_TIMEOUT) {
+        printf("gcoap: timeout for msg ID %02u\n", coap_get_id(pdu));
+        return;
+    }
+    else if (req_state == GCOAP_MEMO_ERR) {
+        printf("gcoap: error in response\n");
+        return;
+    }
+
+    char *class_str = (coap_get_code_class(pdu) == COAP_CLASS_SUCCESS)
+                            ? "Success" : "Error";
+    printf("gcoap: response %s, code %1u.%02u", class_str,
+                                                coap_get_code_class(pdu),
+                                                coap_get_code_detail(pdu));
+    if (pdu->payload_len) {
+        if (pdu->content_type == COAP_FORMAT_TEXT
+                || pdu->content_type == COAP_FORMAT_LINK
+                || coap_get_code_class(pdu) == COAP_CLASS_CLIENT_FAILURE
+                || coap_get_code_class(pdu) == COAP_CLASS_SERVER_FAILURE) {
+            /* Expecting diagnostic payload in failure cases */
+            printf(", %u bytes\n%.*s\n", pdu->payload_len, pdu->payload_len,
+                                                          (char *)pdu->payload);
+        }
+        else {
+            printf(", %u bytes\n", pdu->payload_len);
+            od_hex_dump(pdu->payload, pdu->payload_len, OD_WIDTH_DEFAULT);
+        }
+    }
+    else {
+        printf(", empty payload\n");
+    }
+}
+
+static size_t _send(uint8_t *buf, size_t len, char *addr_str, char *port_str)
+{
+    ipv6_addr_t addr;
+    size_t bytes_sent;
+    sock_udp_ep_t remote;
+
+    remote.family = AF_INET6;
+
+    /* parse for interface */
+    int iface = ipv6_addr_split_iface(addr_str);
+    if (iface == -1) {
+        if (gnrc_netif_numof() == 1) {
+            /* assign the single interface found in gnrc_netif_numof() */
+            remote.netif = (uint16_t)gnrc_netif_iter(NULL)->pid;
+        }
+        else {
+            remote.netif = SOCK_ADDR_ANY_NETIF;
+        }
+    }
+    else {
+        if (gnrc_netif_get_by_pid(iface) == NULL) {
+            puts("gcoap_cli: interface not valid");
+            return 0;
+        }
+        remote.netif = iface;
+    }
+
+    /* parse destination address */
+    if (ipv6_addr_from_str(&addr, addr_str) == NULL) {
+        puts("gcoap_cli: unable to parse destination address");
+        return 0;
+    }
+    if ((remote.netif == SOCK_ADDR_ANY_NETIF) && ipv6_addr_is_link_local(&addr)) {
+        puts("gcoap_cli: must specify interface for link local target");
+        return 0;
+    }
+    memcpy(&remote.addr.ipv6[0], &addr.u8[0], sizeof(addr.u8));
+
+    /* parse port */
+    remote.port = atoi(port_str);
+    if (remote.port == 0) {
+        puts("gcoap_cli: unable to parse destination port");
+        return 0;
+    }
+
+    bytes_sent = gcoap_req_send2(buf, len, &remote, _resp_handler);
+    if (bytes_sent > 0) {
+    }
+    return bytes_sent;
+}
+
+static ssize_t _interval_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx)
+{
+    (void)pdu;
+    (void)buf;
+    (void)len;
+    (void)ctx;
+
+    return -1;
+}
+
+static ssize_t _value_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx)
+{
+    (void)pdu;
+    (void)buf;
+    (void)len;
+    (void)ctx;
+
+    /* read sensor value and write to buffer */
+    // ...
+
+    /* sleep for `interval` */
+    xtimer_sleep(interval);
+
+    return -1;
+}
+
+void spr_init(void)
+{
+    /* Register CoAP resources */
+    gcoap_register_listener(&_listener);
+
+    /* Find RPI/Basisstation */
+    //...
+
+    /* Register Basisstation */
+    //...
+
+    /* Wait configuration from Backend */
+    //...
+
+    /* Configuration finished, wait OBSERVE request from Basisstation */
+}
