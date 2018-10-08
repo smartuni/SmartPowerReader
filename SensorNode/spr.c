0240dedea58893527d4010955f3ed55f0e1cb738
Aiman Ismail <m.aimanismail@gmail.com>
2018-10-08 17:00:24 +0200

    SensorNode: fix const error

--- SensorNode/spr.c
+++ SensorNode/spr.c
@@ -56,7 +56,7 @@ static const coap_resource_t _resources[] = {
 };
 
 static gcoap_listener_t _listener = {
-    &_resources[0],
+    (coap_resource_t *)&_resources[0],
     sizeof(_resources) / sizeof(_resources[0]),
     NULL
 };
