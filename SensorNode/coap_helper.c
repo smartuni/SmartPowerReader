6e7aedd2cf7f6ca7a0d1ac44470928d172b25cd5
Rene Herthel <rene-herthel@web.de>
2018-10-15 13:28:22 +0200

    SensorNode: fix testsend_cmd for sending single measurements

--- SensorNode/coap_helper.c
+++ SensorNode/coap_helper.c
@@ -62,96 +62,95 @@ static void _resp_handler(unsigned req_state, coap_pkt_t* pdu,
 /* Use to send message to remote client/server using CoAP */
 size_t send(uint8_t *buf, size_t len, char *addr_str, char *port_str)
 {
-    ipv6_addr_t addr;
-    size_t bytes_sent;
-    sock_udp_ep_t remote;
-
-    remote.family = AF_INET6;
-
-    /* parse for interface */
-    int iface = ipv6_addr_split_iface(addr_str);
-    if (iface == -1) {
-        if (gnrc_netif_numof() == 1) {
-            /* assign the single interface found in gnrc_netif_numof() */
-            remote.netif = (uint16_t)gnrc_netif_iter(NULL)->pid;
-        }
-        else {
-            remote.netif = SOCK_ADDR_ANY_NETIF;
-        }
-    }
-    else {
-        if (gnrc_netif_get_by_pid(iface) == NULL) {
-            puts("gcoap_cli: interface not valid");
-            return 0;
-        }
-        remote.netif = iface;
-    }
-
-    /* parse destination address */
-    if (ipv6_addr_from_str(&addr, addr_str) == NULL) {
-        puts("gcoap_cli: unable to parse destination address");
-        return 0;
-    }
-    if ((remote.netif == SOCK_ADDR_ANY_NETIF) && ipv6_addr_is_link_local(&addr)) {
-        puts("gcoap_cli: must specify interface for link local target");
-        return 0;
-    }
-    memcpy(&remote.addr.ipv6[0], &addr.u8[0], sizeof(addr.u8));
-
-    /* parse port */
-    remote.port = atoi(port_str);
-    if (remote.port == 0) {
-        puts("gcoap_cli: unable to parse destination port");
-        return 0;
-    }
-
-    bytes_sent = gcoap_req_send2(buf, len, &remote, _resp_handler);
-    if (bytes_sent > 0) {
-    }
-    return bytes_sent;
-}
 
 int testsend_cmd(int argc, char **argv)
 {
-    (void)argc;
+      /* Parameters needed for accurate measurement */
+      ct_param.adc_count = 1 << 12;              // e.g.: 1 << 12 = 4096
+      ct_param.adc_offset = ct_param.adc_count >> 1; // e.g.: 4096 >> 1 = 2048
+      ct_param.v_ref = 3.3;
+      ct_param.r_burden = 110;
+      ct_param.turns = 2000;
+      ct_param.samples = 32; // The number of iterations of the for-loop.
 
-    ct_param.adc_count = 1 << 12;              // e.g.: 1 << 12 = 4096
-    ct_param.adc_offset = ct_param.adc_count >> 1; // e.g.: 4096 >> 1 = 2048
-    ct_param.v_ref = 3.3;
-    ct_param.r_burden = 110;
-    ct_param.turns = 2000;
-    ct_param.samples = 32; // The number of iterations of the for-loop.
+      ct_measure_current(&ct_param, &ct_i_data);
+      ct_dump_current(&ct_i_data);
 
-    /* Ordered like the RFC method code numbers, but off by 1. GET is code 0. */
-    uint8_t buf[GCOAP_PDU_BUF_SIZE];
-    coap_pkt_t pdu;
-    size_t len = 0;
-
-    /* parse options */
-    int apos          = 1;               /* position of address argument */
-    unsigned msg_type = COAP_TYPE_NON;
-
-        gcoap_req_init(&pdu, &buf[0], GCOAP_PDU_BUF_SIZE, COAP_PUT, "/value");
-  /* measure current here and get len */
-        ct_measure_current(&ct_param, &ct_i_data);
-        ct_dump_current(&ct_i_data);
-
-        len = fmt_float((char*)buf, ct_i_data.current, 2);
-        printf("len = %i", len);
+      float current = ct_i_data.current;
 
 
-        memcpy(pdu.payload, buf, len);
-        coap_hdr_set_type(pdu.hdr, msg_type);
 
-        len = gcoap_finish(&pdu, len, COAP_FORMAT_TEXT);
+      /* Ordered like the RFC method code numbers, but off by 1. GET is code 0. */
+      char *method_codes[] = {"get", "post", "put"};
+      uint8_t buf[GCOAP_PDU_BUF_SIZE];
+      coap_pkt_t pdu;
+      size_t len;
 
-        printf("gcoap_cli: sending msg ID %u, %u bytes\n", coap_get_id(&pdu),
-               (unsigned) len);
-        if (!send(&buf[0], len, argv[apos], argv[apos+1])) {
-            puts("gcoap_cli: msg send failed");
-        }
+      char fmt_buf[10] = {""};
+      fmt_float(fmt_buf, current, 2);
+      len = strlen(fmt_buf);
 
-        return 0;
+      if (argc == 1) {
+          /* show help for main commands */
+          goto end;
+      }
+
+      if (strcmp(argv[1], "info") == 0) {
+          printf("CoAP server is listening on port %u\n", GCOAP_PORT);
+          return 0;
+      }
+
+      /* if not 'info', must be a method code */
+      int code_pos = -1;
+      for (size_t i = 0; i < sizeof(method_codes) / sizeof(char*); i++) {
+          if (strcmp(argv[1], method_codes[i]) == 0) {
+              code_pos = i;
+          }
+      }
+      if (code_pos == -1) {
+          goto end;
+      }
+
+      /* parse options */
+      int apos          = 2;               /* position of address argument */
+      unsigned msg_type = COAP_TYPE_NON;
+      if (argc > apos && strcmp(argv[apos], "-c") == 0) {
+          msg_type = COAP_TYPE_CON;
+          apos++;
+      }
+
+      if (argc == apos + 3 || argc == apos + 4) {
+          gcoap_req_init(&pdu, &buf[0], GCOAP_PDU_BUF_SIZE, code_pos+1, argv[apos+2]);
+          if (argc == apos + 4) {
+              memcpy(pdu.payload, fmt_buf, strlen(fmt_buf));
+          }
+          coap_hdr_set_type(pdu.hdr, msg_type);
+
+          if (argc == apos + 4) {
+              len = gcoap_finish(&pdu, strlen(fmt_buf), COAP_FORMAT_TEXT);
+          }
+          else {
+              len = gcoap_finish(&pdu, 0, COAP_FORMAT_NONE);
+          }
+
+          printf("gcoap_cli: sending msg ID %u, %u bytes\n", coap_get_id(&pdu),
+                 (unsigned) len);
+          if (!send(&buf[0], len, argv[apos], argv[apos+1])) {
+              puts("gcoap_cli: msg send failed");
+          }
+          return 0;
+      }
+      else {
+          printf("usage: %s <get|post|put> [-c] <addr>[%%iface] <port> <path> [data]\n",
+                 argv[0]);
+          printf("Options\n");
+          printf("    -c  Send confirmably (defaults to non-confirmable)\n");
+          return 1;
+      }
+
+      end:
+      printf("usage: %s <get|post|put|info>\n", argv[0]);
+      return 1;
 }
 /*
  * This is only used for debugging through the shell.*/
