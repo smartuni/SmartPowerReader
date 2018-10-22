a6698d05042135422138fcc84d42be1fad8bee26
Daniel Kessener <abp403@52-lab03.mailcluster.haw-hamburg.de>
2018-10-22 10:18:05 +0200

    update put

--- Backend/src/spr/resource/MeasurementResource.java
+++ Backend/src/spr/resource/MeasurementResource.java
@@ -3,8 +3,11 @@ package spr.resource;
 import java.nio.ByteBuffer;
 import java.sql.Timestamp;
 
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 
+import dave.util.log.Logger;
+import dave.util.log.Severity;
 import spr.net.common.Gateway;
 import spr.task.Task;
 import spr.task.Tasks;
@@ -19,13 +22,23 @@ public class MeasurementResource extends Resource
 	}
 	
 	@Override
-	public void handlePOST(CoapExchange com)
+	public void handlePUT(CoapExchange com)
 	{
 		String id = com.getSourceAddress().getHostAddress();
-		ByteBuffer bb = ByteBuffer.wrap(com.getRequestPayload());
-		double v = bb.getDouble();
-		long t = (new Timestamp(System.currentTimeMillis())).getTime();
 		
-		getNode().send(Units.DATABASE, new Task(Tasks.Database.STORE, newSession(), new Data(id, t, v)));
+		if(com.getRequestPayload() == null)
+		{
+			Logger.DEFAULT.log(Severity.ERROR, "Client %s send empty payload!", id);
+			
+			com.respond(ResponseCode.BAD_REQUEST);
+		}
+		else
+		{
+			ByteBuffer bb = ByteBuffer.wrap(com.getRequestPayload());
+			double v = bb.getDouble();
+			long t = (new Timestamp(System.currentTimeMillis())).getTime();
+			
+			getNode().send(Units.DATABASE, new Task(Tasks.Database.STORE, newSession(), new Data(id, t, v)));
+		}
 	}
 }
