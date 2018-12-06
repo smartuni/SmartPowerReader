2c515eab8671ad486e89832c34a34ae9e0d760f9
rene herthel <rene-herthel@web.de>
2018-12-06 17:54:27 +0100

    SensorNode/Main: Comment out showing IP at startup

--- SensorNode/main.c
+++ SensorNode/main.c
@@ -18,13 +18,6 @@
 #include "kernel_types.h"
 #include "shell.h"
 
-#include "msg.h"
-#include "net/ipv6/addr.h"
-#include "net/gnrc.h"
-#include "net/gnrc/netif.h"
-
-#include "lcd1602a.h"
-
 #define MAIN_QUEUE_SIZE (4)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
@@ -34,10 +27,6 @@ extern int testsend_cmd(int argc, char **argv);
 extern int gcoap_cli_cmd(int argc, char **argv);
 extern void spr_init(void);
 
-lcd1602a_dev_t lcd;
-lcd1602a_iface_t iface = MODE_4BIT;
-lcd1602a_dotsize_t dotsize = DOTSIZE_5x8;
-
 static const shell_command_t shell_commands[] = {
     { "coap", "CoAP example", gcoap_cli_cmd },
     { "testsend", "test send data", testsend_cmd },
@@ -46,14 +35,47 @@ static const shell_command_t shell_commands[] = {
     { NULL, NULL, NULL }
 };
 
+int main(void)
+{
+    /* for the thread running the shell */
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+    spr_init();
+
+    puts("SmartPowerReader sensor node");
+
+    /*
+    _init_lcd(&lcd);
+    _ip_to_lcd();
+    */
+
+    /* start shell */
+    puts("All up, running the shell now");
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    /* should never be reached */
+    return 0;
+}
+
+/* NOTE: Comment out for the moment. It causes some problems with networking. */
+/*
+#include "msg.h"
+#include "net/ipv6/addr.h"
+#include "net/gnrc.h"
+#include "net/gnrc/netif.h"
+
+#include "lcd1602a.h"
+lcd1602a_dev_t lcd;
+lcd1602a_iface_t iface = MODE_4BIT;
+lcd1602a_dotsize_t dotsize = DOTSIZE_5x8;
+
 static inline void _init_lcd(lcd1602a_dev_t * lcd)
 {
     int PORT_A = 0;
     int PORT_B = 1;
     int PORT_C = 2;
 
-    /* nucleo */
-    /*
+    // Nucleo pin setup
     lcd->register_select_pin = GPIO_PIN(PORT_A, 9);
     lcd->read_write_pin = GPIO_PIN(PORT_A, 8);
     lcd->enable_pin = GPIO_PIN(PORT_C, 7);
@@ -69,9 +91,8 @@ static inline void _init_lcd(lcd1602a_dev_t * lcd)
     lcd->dotsize = dotsize;
     lcd->lines = 2;
     lcd->collumns = 16;
-    */
 
-    /* Phywave board */
+    // Phywave pin setup
     (void)PORT_B;
     int PORT_E = 4;
     lcd->register_select_pin = GPIO_PIN(PORT_E, 4);
@@ -95,11 +116,9 @@ static inline void _init_lcd(lcd1602a_dev_t * lcd)
 
 static inline void _ip_to_lcd(void)
 {
-    /*
-     * NOTE: MAKE SURE TO USE A BOARD WITH NETWORK SUPPORT (or it won't work).
-     */
+    // NOTE: MAKE SURE TO USE A BOARD WITH NETWORK SUPPORT (or it won't work).
 
-    /* get interfaces and print their addresses */
+    // get interfaces and print their addresses
     gnrc_netif_t *netif = NULL;
     while ((netif = gnrc_netif_iter(netif))) {
         printf("netif while \n");
@@ -128,23 +147,4 @@ static inline void _ip_to_lcd(void)
         }
     }
 }
-
-int main(void)
-{
-    /* for the thread running the shell */
-    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
-    spr_init();
-
-    puts("SmartPowerReader sensor node");
-    
-    _init_lcd(&lcd);
-    _ip_to_lcd();
-
-    /* start shell */
-    puts("All up, running the shell now");
-    char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
-
-    /* should never be reached */
-    return 0;
-}
+*/
