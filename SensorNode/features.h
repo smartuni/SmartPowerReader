ee81a9c636f7970ce52f88ce88f7d3ae957e86d6
pokgak <m.aimanismail@gmail.com>
2019-01-07 17:28:14 +0100

    SensorNode: enable display and show ip at startup

--- SensorNode/features.h
+++ SensorNode/features.h
@@ -18,9 +18,9 @@ extern "C" {
 #include "cbor.h"
 
 /* Globaly enable/disable display functionality. */
-#define FEATURE_USE_DISPLAY (0)
+#define FEATURE_USE_DISPLAY (1)
 /* Can only be used with a display */
-#define FEATURE_SHOW_IP_ON_START (0 && FEATURE_USE_DISPLAY)
+#define FEATURE_SHOW_IP_ON_START (1 && FEATURE_USE_DISPLAY)
 /* Turn on and off using buttons. */
 #define FEATURE_USE_BUTTONS (1)
 
